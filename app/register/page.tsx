6327e0b1b3f4edbfa253900d8889f335d895b116
Syed Zaidi <syedqaimali12@gmail.com>
2025-11-02 17:27:11 -0500

    changes pastchat to aikronos

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -28,7 +28,7 @@ export default function RegisterPage() {
         password,
         options: {
           data: { firstName, lastName },
-          emailRedirectTo: "https://pastchat.vercel.app/chat",
+          emailRedirectTo: "https://aikronos.vercel.app/chat",
         },
       });
 
